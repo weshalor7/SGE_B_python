bcfadd651325d7f82214b622ae3f8f311e8c9c90
weshalor7 <2024_imad.ouadi@iticbcn.cat>
2024-11-28 12:20:17 +0100

    cambio a branch activitat-4

--- activitat.py
+++
@@ -1,9 +0,0 @@
-class Cotxe :
-    def __init__(self, marca, modelo, puertas, ruedas, motor):
-        
-        self.marca = marca
-        self.modelo = modelo
-        self.puertas = puertas 
-        self.ruedas = ruedas 
-        self.motor = motor
-
