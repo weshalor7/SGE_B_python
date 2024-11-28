131a163f3d42f7ab8d980cfed2134a487e40e35f
weshalor7 <2024_imad.ouadi@iticbcn.cat>
2024-11-28 12:17:40 +0100

    class cotxe

---
+++ activitat.py
@@ -0,0 +1,9 @@
+class Cotxe :
+    def __init__(self, marca, modelo, puertas, ruedas, motor):
+        
+        self.marca = marca
+        self.modelo = modelo
+        self.puertas = puertas 
+        self.ruedas = ruedas 
+        self.motor = motor
+
---
+++ colibry.py
@@ -0,0 +1,8 @@
+class Colobry :
+    def __init__(self, tipo, puertas, ruedas, motor):
+        
+        self.marca = marca
+        self.modelo = modelo
+        self.puertas = puertas 
+        self.ruedas = ruedas 
+        self.motor = motor
